d675260a814a4a384e48c55048d4abdc7a39e58f
Wiktor Mosakowksi <mosak2205@gmail.com>
2025-10-13 00:07:19 +0200

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("hello world\n");
+    printf("Wiktor Mosakowski\n");
+
+    int liczba;
+    printf("podaj liczbe:\n");
+    scanf("%d", &liczba);
+
+    printf("twoja liczba to: %d\n", liczba);
+    return 0;
+}
